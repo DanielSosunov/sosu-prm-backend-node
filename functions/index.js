8aed98c631d3633636ea7cd5dbef332b03d4bdf7
Daniel <dnlsosunov@gmail.com>
2024-03-12 01:08:12 -0400

    Created process remind function

--- functions/index.js
+++ functions/index.js
@@ -7,17 +7,8 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const { onRequest } = require("firebase-functions/v2/https");
 const functions = require('firebase-functions/v2')
-const logger = require("firebase-functions/logger");
 const { app } = require('./app')
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
 
 exports.process_reminder = functions.https.onRequest(app);
